0fc3e9a06f4ce8eee381ed3b86c0b9b2bda58e4f
charbel-nsr <charbelnasr512@gmail.com>
2025-03-21 14:05:32 -0400

    home hero et header

--- Dataportal/wwwroot/js/site.js
+++ Dataportal/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//header scroll
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header nav');
+    if (window.scrollY > 10) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
