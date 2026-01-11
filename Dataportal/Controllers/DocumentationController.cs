5d780efcfe11672a2c67fd9066140da9aa495f75
charbel-nsr <charbelnasr512@gmail.com>
2026-01-10 21:33:12 -0500

    Add dynamic documentation footer

---
+++ Dataportal/Controllers/DocumentationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dataportal.Controllers
+{
+    public class DocumentationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
