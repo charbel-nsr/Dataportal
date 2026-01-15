af05f162414cc4c4bda21a90b0a7344ca790fb03
charbel-nsr <charbelnasr512@gmail.com>
2026-01-15 18:12:07 -0500

    Implement Launch JupyterHub feature

---
+++ Dataportal/Controllers/JupyterHubLaunchController.cs
@@ -0,0 +1,34 @@
+﻿using Dataportal.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dataportal.Controllers
+{
+    /// <summary>
+    /// Launches JupyterHub with a short-lived SSO token.
+    /// </summary>
+    [Authorize(Roles = "administrator,editor")]
+    [Route("jupyter")]
+    public class JupyterHubLaunchController : Controller
+    {
+        private readonly JupyterSsoTokenService _tokenService;
+
+        public JupyterHubLaunchController(JupyterSsoTokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        /// <summary>
+        /// Redirects authorized users to JupyterHub with a short-lived access token.
+        /// </summary>
+        [HttpGet("launch")]
+        public IActionResult Launch()
+        {
+            var tokenResult = _tokenService.CreateToken(User);
+            var escapedToken = Uri.EscapeDataString(tokenResult.AccessToken);
+            var redirectUrl = $"/jupyterhub/hub/login?token={escapedToken}";
+
+            return Redirect(redirectUrl);
+        }
+    }
+}
